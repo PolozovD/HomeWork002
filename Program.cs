a59cc3fa641f815404cfc89b37d234935deb7b3c
Dmitriy <dpolozov86@gmail.com>
2023-04-29 11:05:43 +0300

    add Completed the task1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int secondDigit (int threeDigitNum)
+{
+    int seconDigitNum = threeDigitNum / 10;
+    int remainder2 = seconDigitNum % 10;
+    return remainder2;
+}
+
+Console.WriteLine("Enter three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = secondDigit(num);
+Console.WriteLine($"{num} the second digit -> {result}");
