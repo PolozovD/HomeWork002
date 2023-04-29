e6675b9e2bb1b2ad707cfe17ec6bb258757fdeab
Dmitriy <dpolozov86@gmail.com>
2023-04-29 12:54:37 +0300

    add Task 2 and solution

--- Program.cs
+++ Program.cs
@@ -1,14 +1,52 @@
 ﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int secondDigit (int threeDigitNum)
+// int secondDigit (int threeDigitNum)
+// {
+//     int seconDigitNum = threeDigitNum / 10;
+//     int remainder2 = seconDigitNum % 10;
+//     return remainder2;
+// }
+
+// Console.WriteLine("Enter three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = secondDigit(num);
+// Console.WriteLine($"{num} the second digit -> {result}");
+
+// 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int threeNum (int number)
 {
-    int seconDigitNum = threeDigitNum / 10;
-    int remainder2 = seconDigitNum % 10;
-    return remainder2;
+    number %= 10;
+    return number;
 }
 
-Console.WriteLine("Enter three-digit number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num = Math.Abs(num2);
 
-int result = secondDigit(num);
-Console.WriteLine($"{num} the second digit -> {result}");
+if(num > 0 && num < 100)
+{
+    Console.WriteLine("there is no third digit");
+}
+if(num > 99 && num < 1000)
+{
+    int result = threeNum(num);
+    Console.WriteLine($"{num} the third num -> {result}");
+}
+if(num > 999 && num < 10000)
+{
+    int num1 = num;
+    num /= 10;
+    int result = threeNum(num);
+    Console.WriteLine($"{num1} the third num -> {result}");
+}
+if(num > 9999)
+{
+    int num1 = num;
+    num /= 100;
+    int result = threeNum(num);
+    Console.WriteLine($"{num1} the third num -> {result}");
+}
+
+
